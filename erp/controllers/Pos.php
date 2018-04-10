27de06342469b775f5fe03b3f1139e3c11ba71ee
Nak <khat.ratanak@cloudnet.com.kh>
2018-04-10 10:18:21 +0700

    Nak upload system

--- erp/controllers/Pos.php
+++ erp/controllers/Pos.php
@@ -731,16 +731,24 @@ class Pos extends MY_Controller
                 $p 		= isset($_POST['amount']) ? sizeof($_POST['amount']) : 0;
 				$p_cur 	= isset($_POST['other_cur_paid']) ? sizeof($_POST['other_cur_paid']) : 0;
 				
+                $g_total= $this->erp->formatDecimal($grand_total);
                 for ($r = 0; $r < $p; $r++) {
 					$pos_b += ($_POST['amount'][$r] + ($_POST['other_cur_paid'][$r]/$cur_rate->rate));
 					$paid  = ($_POST['amount'][$r] + ($_POST['other_cur_paid'][$r]/$cur_rate->rate));
+
+                    if ($pos_b < $g_total) {
+                        $amount = $paid;
+                    } else {
+                        $amount = $g_total - ($pos_b - $paid);
+                    }
+
                     if (isset($_POST['amount'][$r]) && !empty($_POST['amount'][$r]) && isset($_POST['paid_by'][$r]) && !empty($_POST['paid_by'][$r])) {
 						if(strpos($_POST['amount'][$r], '-') !== false){
 							$payment[] = array(
 								'biller_id'				=> $biller_id,
 								'date' 					=> $date,
 								'reference_no' 			=> (($_POST['paid_by'][$r] == 'deposit' || $_POST['paid_by'][$r] == 'depreciation')? $reference : $this->site->getReference('sp')),
-								'amount' 				=> $this->erp->formatDecimal($paid),
+                                'amount'                => $this->erp->formatDecimal($amount),
 								'paid_by' 				=> $_POST['paid_by'][$r],
                                 'cheque_no' 			=> $_POST['cheque_no'][$r],
 								'cc_no' 				=> ($_POST['paid_by'][$r] == 'gift_card' ? $_POST['paying_gift_card_no'][$r] : $_POST['cc_no'][$r]),
@@ -763,7 +771,7 @@ class Pos extends MY_Controller
 								'biller_id'				=> $biller_id,
 								'date' 					=> $date,
 								'reference_no' 			=> (($_POST['paid_by'][$r] == 'deposit' || $_POST['paid_by'][$r] == 'depreciation')? $reference : $this->site->getReference('sp')),
-								'amount' 				=> $this->erp->formatDecimal($paid),
+                                'amount'                => $this->erp->formatDecimal($amount),
 								'paid_by' 				=> $_POST['paid_by'][$r],
 								'cheque_no' 			=> $_POST['cheque_no'][$r],
 								'cc_no' 				=> ($_POST['paid_by'][$r] == 'gift_card' ? $_POST['paying_gift_card_no'][$r] : $_POST['cc_no'][$r]),
@@ -799,7 +807,8 @@ class Pos extends MY_Controller
 								'biller_id'				=> $biller_id,
 								'date' 					=> $date,
 								'reference_no' 			=> (($_POST['paid_by'][$j] == 'deposit' || $_POST['paid_by'][$j] == 'depreciation')? $reference : $this->site->getReference('sp')),
-								'amount' 				=> $this->erp->formatDecimal($paidi),
+								//'amount' 				=> $this->erp->formatDecimal($paidi),
+                                'amount'                => $this->erp->formatDecimal($amount),
 								'paid_by' 				=> $_POST['paid_by'][$j],
 								'cheque_no' 			=> $_POST['cheque_no'][$j],
 								'cc_no' 				=> ($_POST['paid_by'][$j] == 'gift_card' ? $_POST['paying_gift_card_no'][$j] : $_POST['cc_no'][$j]),
